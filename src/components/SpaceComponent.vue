6e8116c82394707b53e60ee987349af2dfc89443
Troy Fuhriman <TkFuhriman@gmail.com>
2020-06-02 17:03:13 -0600

    card changes

--- src/components/SpaceComponent.vue
+++ src/components/SpaceComponent.vue
@@ -2,7 +2,7 @@
   <div class="spaceComponent col-md-6 col-12 text-center">
     <div class="card shadow border border-dark">
       <img class="card-img-top" :src="space.hdurl" alt />
-      <div class="card-body">
+      <div class="card-body bg-white">
         <h4 class="card-title">{{space.title}}</h4>
         <p class="card-text">{{space.explanation}}</p>
       </div>
